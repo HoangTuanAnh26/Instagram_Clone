c1b24a2535e4587b3415b0081a6eb8fc4da41c9b
hoangtuananh <tuanhoanga789@gmail.com>
2026-04-26 08:03:02 +0900

    model

---
+++ my-app/resources/views/posts/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>投稿一覧</title>
+</head>
+<body>
+    <h1>投稿一覧</h1>
+    @foreach ($posts as $post)
+    <div>
+        <h2>{{ $post->title }}</h2>
+        <p>{{ $post->body }}</p>
+    </div>
+    @endforeach
+    
+</body>
+</html>
